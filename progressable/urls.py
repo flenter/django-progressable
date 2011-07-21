af623cd252fc4c884ec9beecc2a0f73bfe4df46a
Jacco Flenter <jacco@secretcodemachine.com>
2011-07-21 11:46:44 +0200

    patterns error fix

--- progressable/urls.py
+++ progressable/urls.py
@@ -1,5 +1,16 @@
 from progressable.api import TaskStatusResource
+#from progressable import api
+
+#tsr = api.TaskStatusResource()
+
+#print tsr.urls
 
 status_resource = TaskStatusResource()
 
-patterns = status_resource.urls.patterns
+from django.conf.urls.defaults import patterns, url, include
+
+
+urlpatterns = patterns('',
+        url(r'', include(status_resource.urls)),)
+
+#status_resource.urls.patterns
