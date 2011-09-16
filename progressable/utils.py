6bd2492f3fd971e10ef9eca9ba33a39450792017
Jacco Flenter <github@putpixel.nl>
2011-09-16 15:15:32 +0300

    site added to the register_task function. TaskStatus will get the value of the Site.objects.get_current().domain

--- progressable/utils.py
+++ progressable/utils.py
@@ -2,13 +2,16 @@ from progressable.models import TaskStatus as TaskStatus
 from progressable.states import PROGRESS
 
 def register_task(result, staff_required = False, hidden = True, title=''):
+    
+    from django.contrib.sites.models import Site
 
     t_status = TaskStatus(
         staff_required = staff_required,
         task_id = result.task_id,
         hidden = hidden, 
         task_name = result.task_name,
-        title=title
+        title=title,
+        site = Site.objects.get_current().domain
     )
 
     t_status.save()
